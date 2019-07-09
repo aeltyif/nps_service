35d99abf6390b44e8d62469784551217e9e62c20
Ahmad Albu-Eltyif <a.albueltyif@boundlessdrop.com>
2019-07-09 18:06:10 +0300

    Fixes for the swagger file

--- spec/integration/net_promoter_scores_spec.rb
+++ spec/integration/net_promoter_scores_spec.rb
@@ -6,25 +6,9 @@ describe 'Net Promoter Scores API' do
     get 'List all net promoter scores' do
       tags 'Net Promoter Scores'
       consumes 'application/json'
-      response '200', 'Net promoter scores found' do
-        let(:id) { net_promoter_score.create(score: 10,
-                                             touchpoint: 'realtor_feedback',
-                                             respondent_type: 'Seller' ,
-                                             respondent_id: create(:seller, name: 'Hello Respondent').id ,
-                                             object_type: 'Realtor' ,
-                                             object_id: create(:realtor, name: 'Hello Realtor').id).id }
-        run_test!
-      end
-
-      response '400', 'Invalid request' do
-        let(:net_promoter_score) { { score: 11 } }
-        run_test!
-      end
-
-      response '422', 'Unprocessable Entity' do
-        let(:net_promoter_score) { { score: 11 } }
-        run_test!
-      end
+      response '200', 'Net promoter scores found'
+      response '400', 'Invalid request'
+      response '422', 'Unprocessable Entity'
     end
   end
 
@@ -45,25 +29,9 @@ describe 'Net Promoter Scores API' do
         required: %w[score touchpoint respondent_type respondent_id object_type object_id]
       }
 
-      response '201', 'Net promoter score created' do
-        let(:net_promoter_score) { { score: 10,
-                                     touchpoint: 'realtor_feedback',
-                                     respondent_type: 'Seller' ,
-                                     respondent_id: create(:seller, name: 'Hello Respondent').id ,
-                                     object_type: 'Realtor' ,
-                                     object_id: create(:realtor, name: 'Hello Realtor').id } }
-        run_test!
-      end
-
-      response '400', 'Invalid request' do
-        let(:net_promoter_score) { { score: 11 } }
-        run_test!
-      end
-
-      response '422', 'Unprocessable Entity' do
-        let(:net_promoter_score) { { score: 11 } }
-        run_test!
-      end
+      response '201', 'Net promoter score created'
+      response '400', 'Invalid request'
+      response '422', 'Unprocessable Entity'
     end
   end
 end
